37910aa8c141aa92d78dd82e8ccc72f296c71f2d
Corneil du Plessis <corneil.duplessis@gmail.com>
2019-07-26 22:08:01 +0200

    added memory configuration to run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ node --version
 /usr/bin/time -v node --max-old-space-size=12 build/js/node_modules/kfsm-samples/kotlin/kfsm-samples.js
 echo "JVM Version"
 java -version
-/usr/bin/time -v java -Xmx2m -jar ./build/libs/kfsm-samples-fat-LOCAL-SNAPSHOT.jar
+/usr/bin/time -v java  -Xmx36m -jar ./build/libs/kfsm-samples-fat-LOCAL-SNAPSHOT.jar
 echo "Native Release Version"
 /usr/bin/time -v ./build/bin/native/releaseExecutable/kfsm-samples.kexe
 echo "Native Debug Version"
@@ -12,5 +12,5 @@ echo "Native Debug Version"
 if [[ -f ./build/native/kfsm-samples ]]
 then
     echo "Graal Native Image Version"
-    /usr/bin/time -v ./build/native/kfsm-samples -Xmx1m
+    /usr/bin/time -v ./build/native/kfsm-samples -Xmx36m
 fi
